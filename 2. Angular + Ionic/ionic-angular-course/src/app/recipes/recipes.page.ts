11be778f5fb6e7a1e4209642f66ae28609556588
PranavBhatia <bhatiapranav1996@gmail.com>
2020-05-26 20:19:38 -0400

    Managing State with Services

--- 2. Angular + Ionic/ionic-angular-course/src/app/recipes/recipes.page.ts
+++ 2. Angular + Ionic/ionic-angular-course/src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from "./recipe.model";
+import {RecipesService} from "./recipes.service";
 
 @Component({
     selector: 'app-recipes',
@@ -7,24 +8,15 @@ import {Recipe} from "./recipe.model";
     styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-    recipes: Recipe[] = [
-        {
-            id: 'r1',
-            title: 'Schnitzel',
-            imageUrl: 'https://assets.bonappetit.com/photos/57ae1afd53e63daf11a4e26f/2:1/w_880,c_limit/chicken-schnitzel.jpg?mbid=social_retweet',
-            ingredients: ['French fries', 'Chicken', 'Salad']
-        },
-        {
-            id: 'r1',
-            title: 'Spaghetti',
-            imageUrl: 'https://www.errenskitchen.com/wp-content/uploads/2015/02/Quick-Easy-Spaghetti-Bolognese2-1.jpg',
-            ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
-        }
-    ];
-    constructor() {
+    recipes: Recipe[];
+
+    constructor(private recipesService: RecipesService) {
     }
 
     ngOnInit() {
+        this.recipes = this.recipesService.getAllRecipes();
     }
+
+
 
 }
