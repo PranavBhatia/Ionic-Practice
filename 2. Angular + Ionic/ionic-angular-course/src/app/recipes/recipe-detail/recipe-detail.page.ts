91b25ed933327a578297286b326092c2e04980ca
PranavBhatia <bhatiapranav1996@gmail.com>
2020-05-27 21:28:44 -0400

    Deleting a Recipe

--- 2. Angular + Ionic/ionic-angular-course/src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ 2. Angular + Ionic/ionic-angular-course/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {RecipesService} from "../recipes.service";
 import {Recipe} from "../recipe.model";
 
@@ -13,7 +13,8 @@ export class RecipeDetailPage implements OnInit {
 
   constructor(
       private activatedRoute: ActivatedRoute,
-      private recipesService: RecipesService
+      private recipesService: RecipesService,
+      private router: Router
   ) {
   }
 
@@ -27,4 +28,8 @@ export class RecipeDetailPage implements OnInit {
     });
   }
 
+  onDeleteRecipe() {
+    this.recipesService.deleteRecipe(this.recipe.id);
+    this.router.navigate(['/recipes']).then();
+  }
 }
