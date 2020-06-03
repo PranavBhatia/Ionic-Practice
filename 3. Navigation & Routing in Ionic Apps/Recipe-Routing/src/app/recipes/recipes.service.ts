ed00b76aeca60b4051c0a3b1e611ab1f469ff736
PranavBhatia <bhatiapranav1996@gmail.com>
2020-06-02 21:03:55 -0400

    Start new section

---
+++ 3. Navigation & Routing in Ionic Apps/Recipe-Routing/src/app/recipes/recipes.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {Recipe} from "./recipe.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://assets.bonappetit.com/photos/57ae1afd53e63daf11a4e26f/2:1/w_880,c_limit/chicken-schnitzel.jpg?mbid=social_retweet',
+      ingredients: ['French fries', 'Chicken', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://www.errenskitchen.com/wp-content/uploads/2015/02/Quick-Easy-Spaghetti-Bolognese2-1.jpg',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+    }
+  ];
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {...this.recipes.find(recipe => recipe.id === recipeId)};
+  }
+
+  deleteRecipe(recipeId: string) {
+    this.recipes = this.recipes.filter(recipe => recipe.id !== recipeId);
+  }
+}
